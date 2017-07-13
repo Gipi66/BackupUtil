485a3a6874270ed2f58f5a85f11353c80c807f3b
sergey <martin10@bk.ru>
2017-07-13 17:53:34 +0300

    add unpack config

--- src/main/java/ua/ci/file_replacer/Main.java
+++ src/main/java/ua/ci/file_replacer/Main.java
@@ -3,8 +3,18 @@
  */
 package ua.ci.file_replacer;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.RandomAccessFile;
+import java.nio.channels.Channels;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Date;
+import java.util.Properties;
 
 /**
  * @author pc2
@@ -16,9 +26,11 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		Properties props = loadProps();
 		long startTime = new Date().getTime();
 		ReplacerThread repl = new ReplacerThread("/home/pc2/realt/realtyboard/media", "/home/pc2/del/media", true, true,
-				true);
+				"", "media", props);
+
 		// repl.run();
 		try {
 			repl.compress();
@@ -26,7 +38,68 @@ public class Main {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+
+		repl.decompress();
 		System.out.println("total time: " + new Date(new Date().getTime() - startTime).getTime() / 1000);
 	}
 
+	public static Properties loadProps() {
+		Properties props = new Properties();
+		Path propsPath = Paths.get("conf.ini");
+		if (Files.notExists(propsPath)) {
+			createProps();
+		}
+		try {
+			props.load(new FileInputStream("conf.ini"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		String[] reqKeys = new String[] { "directory_path", "outfile_path", "filter_name", "main_dir_name",
+				"archive_path", "extract_to_path" };
+		for (String key : reqKeys) {
+			if (!props.containsKey(key)) {
+				System.out.printf(
+						"Error: Please add a \"%s\" to the configuration file and try run again.\nTo set the default configuration, delete \"%s\" file.",
+						key, propsPath.toAbsolutePath().toString());
+
+				System.exit(1);
+			}
+		}
+		return props;
+	}
+
+	public static void createProps() {
+		File file = new File("conf.ini");
+
+		RandomAccessFile raf;
+		OutputStream out = null;
+		try {
+			raf = new RandomAccessFile(file, "rws");
+			out = Channels.newOutputStream(raf.getChannel());
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Properties compressProps = new Properties();
+		Properties deCompressProps = new Properties();
+
+		compressProps.put("directory_path", "//home//pc2//realt//realtyboard//media");
+		compressProps.put("outfile_path", "//home//pc2//del//arch//media.tar.gz");
+		compressProps.put("filter_name", "");
+		compressProps.put("main_dir_name", "media");
+
+		deCompressProps.put("archive_path", "//home//pc2//del//arch//media.tar.gz");
+		deCompressProps.put("extract_to_path", "//home//pc2//del//arch//media");
+
+		try {
+			compressProps.store(out, "Configuration for creat archive");
+			deCompressProps.store(out, "Configuration for creat archive");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
 }
--- src/main/java/ua/ci/file_replacer/ReplacerThread.java
+++ src/main/java/ua/ci/file_replacer/ReplacerThread.java
@@ -2,31 +2,27 @@ package ua.ci.file_replacer;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
+import java.io.OutputStream;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
+import java.util.Properties;
 import java.util.logging.Logger;
 
-import org.apache.commons.compress.archivers.sevenz.SevenZArchiveEntry;
-import org.apache.commons.compress.archivers.sevenz.SevenZMethod;
-import org.apache.commons.compress.archivers.sevenz.SevenZOutputFile;
+import org.apache.commons.compress.archivers.ArchiveEntry;
 import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
 import org.apache.commons.compress.archivers.tar.TarArchiveInputStream;
 import org.apache.commons.compress.archivers.tar.TarArchiveOutputStream;
+import org.apache.commons.compress.compressors.gzip.GzipCompressorInputStream;
 import org.apache.commons.compress.compressors.gzip.GzipCompressorOutputStream;
 import org.apache.commons.compress.utils.IOUtils;
-
-import static java.lang.System.out;
 
 public class ReplacerThread {
 	private final Path inputDir;
@@ -34,8 +30,15 @@ public class ReplacerThread {
 
 	private boolean hasRecursive;
 	private boolean hasReplace;
-	private boolean onlyThumbs;
+	private boolean isFilter;
 
+	private String filter;
+
+	private String mediaName;
+	private boolean isMedia;
+
+	private String archive_path;
+	private String extract_to_path;
 	private ArrayList<Path> files;
 
 	public ReplacerThread(String oldDirPath, String newDirPath) {
@@ -45,31 +48,70 @@ public class ReplacerThread {
 		files = new ArrayList<Path>();
 	}
 
-	public ReplacerThread(String oldDirPath, String newDirPath, boolean hasRecursive, boolean hasReplace,
-			boolean onlyThumbs) {
-		this(oldDirPath, newDirPath);
+	public ReplacerThread(String oldDirPath, String newDirPath, boolean hasRecursive, boolean hasReplace, String filter,
+			String mediaName, Properties props) {
+
+		this(props.getProperty("directory_path"), props.getProperty("outfile_path"));
+		// TODO
 		this.hasRecursive = hasRecursive;
+		// TODO
 		this.hasReplace = hasReplace;
-		this.onlyThumbs = onlyThumbs;
+
+		this.filter = props.getProperty("filter_name");
+		this.isFilter = (this.filter.equals(null) || this.filter.equals("")) ? false : true;
+
+		this.mediaName = props.getProperty("main_dir_name");
+		this.isMedia = (this.mediaName.equals(null) || this.mediaName.equals("")) ? false : true;
+
+		this.archive_path = props.getProperty("archive_path");
+		this.extract_to_path = props.getProperty("extract_to_path");
+
 	}
 
 	public void compress() throws IOException {
 		runListFiles();
-
 		createTarGZ(Paths.get("//home//pc2//del//arch//media.tar.gz"));
+	}
 
+	public void decompress() {
+		decompressTarGz();
+	}
+
+	private void decompressTarGz() {
+		try (FileInputStream fin = new FileInputStream(archive_path);
+				BufferedInputStream bis = new BufferedInputStream(fin);
+				GzipCompressorInputStream gzIn = new GzipCompressorInputStream(bis);
+				TarArchiveInputStream tarIn = new TarArchiveInputStream(gzIn);) {
+
+			ArchiveEntry entry = null;
+
+			while ((entry = tarIn.getNextTarEntry()) != null) {
+				File curfile = new File(extract_to_path, entry.getName());
+				log.info(curfile.toString());
+				File parent = curfile.getParentFile();
+				if (!parent.exists()) {
+					parent.mkdirs();
+				}
+				OutputStream out = new FileOutputStream(curfile);
+				IOUtils.copy(tarIn, out);
+				out.close();
+			}
+
+			fin.close();
+
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+		}
 	}
 
 	public void createTarGZ(Path file) throws FileNotFoundException, IOException {
-		// FileOutputStream fOut = null;
-		// BufferedOutputStream bOut = null;
-		// GzipCompressorOutputStream gzOut = null;
-		// TarArchiveOutputStream tOut = null;fsdfsdfsdf
 
-		System.out.println(new File(".").getAbsolutePath());
-		System.out.println(new File(".").getAbsolutePath());
-		String tarGzPath = "//home//pc2//del//arch//media.tar.gz";
-		try (final FileOutputStream fOut = new FileOutputStream(new File(tarGzPath));
+		try (final FileOutputStream fOut = new FileOutputStream(new File(archive_path));
 				final BufferedOutputStream bOut = new BufferedOutputStream(fOut);
 				final GzipCompressorOutputStream gzOut = new GzipCompressorOutputStream(bOut);
 				final TarArchiveOutputStream tOut = new TarArchiveOutputStream(gzOut);) {
@@ -93,43 +135,20 @@ public class ReplacerThread {
 		}
 	}
 
-	private void addFileToTarGz(TarArchiveOutputStream tOut, String path, String base) throws IOException {
-		File f = new File(path);
-		System.out.println(f.exists());
-		String entryName = base + f.getName();
-		TarArchiveEntry tarEntry = new TarArchiveEntry(f, entryName);
-		tOut.putArchiveEntry(tarEntry);
-
-		if (f.isFile()) {
-			IOUtils.copy(new FileInputStream(f), tOut);
-			tOut.closeArchiveEntry();
-		} else {
-			tOut.closeArchiveEntry();
-			File[] children = f.listFiles();
-			if (children != null) {
-				for (File child : children) {
-					System.out.println(child.getName());
-					addFileToTarGz(tOut, child.getAbsolutePath(), entryName + "/");
-				}
-			}
-		}
-	}
-
 	private void addFileToTarGz2(TarArchiveOutputStream tOut, Path filePath, String base) throws IOException {
 
-		String entryName = base + filePath.getFileName();
-		TarArchiveEntry tarEntry = new TarArchiveEntry(filePath.toFile(), entryName);
 		String filePostfix = "";
 		{
-			int fileSubCount = filePath.getNameCount();
-			int subStart = fileSubCount - 5;
-			if (!filePath.toString().contains("thumb")) {
-				subStart++;
+
+			filePostfix = filePath.toString();
+			if (isMedia) {
+				String[] filePostfixSplit = filePostfix.split(mediaName);
+				filePostfix = filePostfixSplit[filePostfixSplit.length - 1];
 			}
-			filePostfix = filePath.subpath(subStart, fileSubCount).toString();
 		}
 
-		tarEntry.setName(filePostfix);
+		TarArchiveEntry tarEntry = new TarArchiveEntry(filePath.toFile(), filePostfix);
+
 		tOut.putArchiveEntry(tarEntry);
 
 		if (Files.exists(filePath) && Files.isRegularFile(filePath)) {
@@ -151,7 +170,7 @@ public class ReplacerThread {
 			{
 				int fileSubCount = filePath.getNameCount();
 				int subStart = fileSubCount - 5;
-				if (!filePath.toString().contains("thumb")) {
+				if (!filePath.toString().contains(filter)) {
 					subStart++;
 				}
 				String filePostfix = filePath.subpath(subStart, fileSubCount).toString();
@@ -187,7 +206,7 @@ public class ReplacerThread {
 				e.printStackTrace();
 			}
 		} else if (Files.isRegularFile(inputDir)) {
-			if (onlyThumbs) {
+			if (isFilter) {
 				if (inputDir.toString().contains("thumbs")) {
 					files.add(inputDir);
 				}
@@ -210,8 +229,8 @@ public class ReplacerThread {
 						}
 					} else if (!Files.isDirectory(entry)) {
 
-						if (onlyThumbs) {
-							if (entry.toString().contains("thumbs")) {
+						if (isFilter) {
+							if (entry.toString().contains(filter)) {
 								files.add(entry);
 							}
 						} else {
